de6ac4fdceff2802989340a734798bb4553a9be6
KamenDamov <kamendamov123@gmail.com>
2023-01-05 18:11:04 -0500

    push

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,11 +3,11 @@ import "./index.css";
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height:'100%' }}>
+    <div class="allElems">
       <h1>What type of investor are you?</h1>
-      <div className = "allElems">
-        <button className='buttons'>I want to invest in something safe</button>
-        <button className='buttons'>High risk, high reward</button>
+      <div class = "button-container">
+        <button class='buttons'>I want to invest in something safe</button>
+        <button class='buttons'>High risk, high reward</button>
       </div>
     </div>
   );
