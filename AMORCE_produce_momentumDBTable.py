92f7896f097d185bfa2ccafb9d01d4fb8e8dad02
KamenDamov <kamendamov123@gmail.com>
2023-03-01 15:13:54 -0500

    push

--- AMORCE_produce_momentumDBTable.py
+++ AMORCE_produce_momentumDBTable.py
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup as bs
 from scipy import stats
 import psycopg2
 import time
+from sqlalchemy import create_engine
 
 # Connect to the PostgreSQL server
 conn = psycopg2.connect(
@@ -55,8 +56,105 @@ def get_hurst_exponent(time_series, max_lag):
 
     return reg[0]
 
-# Execute a CREATE TABLE statement to create a new table
-cur.execute(momentum_query)
+cur.execute("SELECT Ticker FROM tickers")
+tick = [row[0] for row in cur.fetchall()]
+momentum_cols = ['Ticker','Price',
+                'y1_Price_Return','y1_percentile', 
+                'm6_Price_Return', 'm6_percentile', 
+                'm3_Price_Return', 'm3_percentile',
+                'm1_Price_Return','m1_percentile',
+                'Weighted_Hurst_Exponent']
+momentum = pd.DataFrame(columns = momentum_cols)
+
+count = 0 
+for i in range(len(tick)): 
+    start = time.time()    
+    try:
+        #Creating the df to be added to all_time_prices
+        ticker = tick[i]
+        info = yf.Ticker(ticker).history(period='max')
+        info.reset_index(inplace = True)
+        info['Ticker'] = ticker
+
+        info.sort_values("Date", ascending = True, inplace = True)
+        info.set_index("Date", inplace = True)
+        #Creating the df to be added to momentum
+        y = info.truncate(before=pd.Timestamp('today') - pd.DateOffset(months=12))
+        m6 = info.truncate(before=pd.Timestamp('today') - pd.DateOffset(months=6))
+        m3 = info.truncate(before=pd.Timestamp('today') - pd.DateOffset(months=3))
+        m1 = info.truncate(before=pd.Timestamp('today') - pd.DateOffset(months=1))
+
+        #momentum['Ticker'] = tick['0'][i]  
+        try: 
+            perc_change1y = ((y.iloc[-1]['Close'] - y.iloc[0]['Close'])/y.iloc[0]["Close"])
+            #momentum['1y Price Return'] = perc_change1y
+            perc_change6m = ((m6.iloc[-1]['Close'] - m6.iloc[0]['Close'])/m6.iloc[0]["Close"])
+            #momentum['6m Price Return'] = perc_change6m
+            perc_change3m = ((m3.iloc[-1]['Close'] - m3.iloc[0]['Close'])/m3.iloc[0]["Close"])
+            #momentum['3m Price Return'] = perc_change3m
+            perc_change1m = ((m1.iloc[-1]['Close'] - m1.iloc[0]['Close'])/m1.iloc[0]["Close"])
+            #momentum['1m Price Return'] = perc_change1m
+
+            #momentum['Price'] = info['Close'] 
+            #momentum['1y percentile'] = 0
+            #momentum['6m percentile'] = 0
+            #momentum['3m percentile'] = 0
+            #momentum['1m percentile'] = 0
+            #momentum['Weighted Hurst Exponent'] = 0
+            t = [20, 100, 300, 500, 1000]
+            total_hurst = 0
+            weight = 0.4
+            for j in range(len(t)): 
+                #Use Weighted average to determine hurst exp of the stock
+                hurst_exp = get_hurst_exponent(info["Close"].values, t[j])
+                #print(tick["0"][i] + " Hurst exponent with " + str(t[j]) +  ' lags: ' + str(hurst_exp))
+                total_hurst += hurst_exp*weight
+                if j >= 2: 
+                    weight = 0.1
+                else: 
+                    weight = 0.2
+            #momentum = momentum.iloc[-1]
+        except IndexError: 
+            print('Index Error when producing momentum table')
+            continue
+        momentum_append = pd.Series([tick[i],info['Close'][-1],perc_change1y,0,perc_change6m,0,perc_change3m,0,perc_change1m,0, total_hurst],
+                                index = momentum_cols)
+        #print(momentum_append)
+    except TypeError: 
+        print("Nonetype found for: " + tick[i])
+        continue
+    except IndexError: 
+        print("Couldn't find: ",tick[i])
+        continue
+    except KeyError:
+        print("Couldnt find key for: " + tick[i])
+        continue
+    stop = time.time()
+    duration = stop-start
+    print(duration)
+    momentum.loc[len(momentum)] = momentum_append
+
+#Get percentiles for each stock
+cols = ['y1', 'm6', 'm3', 'm1']
+for c in cols: 
+    for index, row in momentum.iterrows(): 
+        percentile_change = stats.percentileofscore(momentum[c + '_Price_Return'], momentum[c + '_Price_Return'].loc[index])
+        momentum[c + '_percentile'][index] = percentile_change 
+#momentum['Shares To Buy'] = 0
+    
+#Calculate HQM score
+#Get the mean of all 4 percentiles 
+momentum['HQM_score'] = 0
+from statistics import mean
+for index, row in momentum.iterrows(): 
+    all_periods_p = [row['y1_percentile'], row['m6_percentile'], row['m3_percentile'], row['m1_percentile']]
+    momentum['HQM_score'].iloc[index] = mean(all_periods_p)
+
+print(momentum.columns)
+
+#PUSH TO DATABASE
+engine = create_engine('postgresql+psycopg2://', creator=lambda: conn)
+momentum.to_sql(name='momentum', con=engine, if_exists='replace', index=False)
 
 # Commit the transaction
 conn.commit()
