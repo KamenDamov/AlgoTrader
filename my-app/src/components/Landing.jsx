a377102f8211ccb81fcca372a4cc848a07d80e8f
KamenDamov <kamendamov123@gmail.com>
2023-03-02 15:41:26 -0500

    push

--- my-app/src/components/Landing.jsx
+++ my-app/src/components/Landing.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
+import './style/landing.css';
 
 const Landing = () => {
   return (
-    <div>
+    <div className="landing-container">
       <h2>
           Test your <span>AI and algorithmic trading strategy</span> today
       </h2>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './components/style/signup.css';
 import './components/style/login.css';
 import Landing from './components/Landing';
@@ -9,11 +9,11 @@ import SignUp from './components/SignUp';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/Login" component={Login} />
-        <Route path="/SignUp" component={SignUp} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/SignUp" element={<SignUp/>} />
+      </Routes>
     </Router>
   );
 }
