d92dbfa88e3f38d63ba496453bb63adc52f02d1d
KamenDamov <kamendamov123@gmail.com>
2023-03-02 17:23:17 -0500

    push

--- my-app/src/components/Landing.jsx
+++ my-app/src/components/Landing.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './style/landing.css';
+import React from 'react';
 
 const Landing = () => {
   return (
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './components/style/signup.css';
-import './components/style/login.css';
-import Landing from './components/Landing';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import { createRoot } from 'react-dom/client';
+import App from './components/Main';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+createRoot(document.getElementById('root')).render(<App />);
