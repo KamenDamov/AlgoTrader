76e56e1d6954761cb4bca856c5304bef5f899395
KamenDamov <kamendamov123@gmail.com>
2023-03-04 17:26:28 -0500

    push

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './style/signup.css';
 import './style/login.css';
 import Landing from './Landing';
 import Login from './LoginForm';
 import SignUp from './SignUp';
-import UserData from './UserData';
+import UserDashboard from './UserDashboard';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  const handleLogin = (data) => {
+    setUserData(data);
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setUserData(null);
+    setLoggedIn(false);
+  };
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Landing/>} />
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/SignUp" element={<SignUp/>} />
-        <Route path="/UserData" element={<UserData/>} />
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/Login" element={<Login handleLogin={handleLogin} />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        {loggedIn ? (
+          <Route
+            path="/UserDashboard"
+            element={<UserDashboard userData={userData} handleLogout={handleLogout} />}
+          />
+        ) : null}
       </Routes>
     </Router>
   );
---
+++ my-app/src/components/UserDashboard.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function UserDashboard() {
+  const [userData, setUserData] = useState(null);
+  const [newFunds, setNewFunds] = useState(0);
+
+  useEffect(() => {
+    // Query user data from the server using axios
+    axios.get('/getUserData').then((response) => {
+      setUserData(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+  const handleFundsChange = (event) => {
+    setNewFunds(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Modify the user's funds attribute in the database using axios
+    axios.post('/modifyFunds', { funds: newFunds }).then((response) => {
+      setUserData(response.data);
+      setNewFunds(0);
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+
+  return (
+    <div>
+      {userData ? (
+        <div>
+          <h1>Welcome, {userData.username}!</h1>
+          <p>Your current funds: ${userData.funds}</p>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Add Funds:
+              <input type="number" value={newFunds} onChange={handleFundsChange} />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      ) : (
+        <p>Loading user data...</p>
+      )}
+    </div>
+  );
+}
+
+export default UserDashboard;
