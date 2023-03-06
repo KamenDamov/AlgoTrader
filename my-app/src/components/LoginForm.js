0897021aaec044511fabc60cd3836d68f548f733
KamenDamov <kamendamov123@gmail.com>
2023-03-06 12:48:51 -0500

    push

--- my-app/src/components/LoginForm.js
+++ my-app/src/components/LoginForm.js
@@ -1,37 +1,36 @@
-import {Link, useNavigate} from 'react-router-dom'
-import React, { useState } from "react";
-import axios from 'axios';
-import "./style/login.css";
-import './style/global.css';
-
-
 function LoginForm() {
   const navigate = useNavigate();
   const [name, setName] = useState("");
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+
+    if (!name || !password) {
+      setError("Please enter your username and password");
+      return;
+    }
+
     const user = { name, password };
 
-  axios.post("http://localhost:3001/login", user)
-    .then((response) => {
-      console.log(response.data);
-      console.log("Logged in")
-      navigate('/UserDashboard');
-      // Store the access token in frontend state or in a cookie/local storage
-    })
-    .catch((error) => {
-      console.error(error);
-      console.log("Can't log in")
-    });
-    };
+    axios.post("http://localhost:3001/login", user)
+      .then((response) => {
+        console.log(response.data);
+        console.log("Logged in");
+        navigate('/UserDashboard');
+        // Store the access token in frontend state or in a cookie/local storage
+      })
+      .catch((error) => {
+        console.error(error);
+        setError("Unable to log in. Please try again.");
+      });
+  };
 
   return (
     <div className="hero-image">
       <div className="form-container">
-        
+        {error && <p className="error">{error}</p>}
         <form onSubmit={handleSubmit}>
           <label htmlFor="name">Username</label>
           <input
@@ -50,7 +49,7 @@ function LoginForm() {
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
-          
+
           <button type="login">Log in</button>
         </form>
         <Link to="/" className="back-to-landing-page">Back to landing page</Link>
@@ -58,5 +57,3 @@ function LoginForm() {
     </div>
   );
 }
-
-export default LoginForm;
