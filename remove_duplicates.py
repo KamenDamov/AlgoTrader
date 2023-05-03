65dacf20b2fca94f671d30af192586a23573a1eb
KamenDamov <kamendamov123@gmail.com>
2023-05-03 15:52:29 -0400

    push

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,14 +1,31 @@
 #Script to remove duplicates
 import psycopg2
 
+# Connect to the PostgreSQL server
+conn = psycopg2.connect(
+    host="localhost",
+    database="financial_db",
+    user="postgres",
+    password="KaMendiNiO"
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
 query = """
 -- create a temporary table with unique values
 CREATE TEMPORARY TABLE temp_table AS 
-SELECT DISTINCT * FROM your_table_name;
+SELECT DISTINCT * FROM all_time_prices;
 
 -- delete the original table
-DROP TABLE your_table_name;
+DROP TABLE all_time_prices;
 
 -- rename the temporary table to the original table name
-ALTER TABLE temp_table RENAME TO your_table_name;
+ALTER TABLE temp_table RENAME TO all_time_prices;
 """
+
+cur.execute(query)
+
+conn.commit()
+cur.close()
+conn.close()
