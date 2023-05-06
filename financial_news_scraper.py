986729ba74db7d9f940e0f98dcc4e38507da6735
KamenDamov <kamendamov123@gmail.com>
2023-05-06 00:18:08 -0400

    push

--- financial_news_scraper.py
+++ financial_news_scraper.py
@@ -7,6 +7,10 @@ from bs4 import BeautifulSoup as bs
 from datetime import datetime
 from dateutil import parser
 
+
+#TODO
+#Add for loop that iterates all stocks in db
+# Produce record and push to db 
 url = "https://www.marketwatch.com/investing/stock/aapl"
 target = "marketshare"
 data = requests.get(url).content
@@ -22,7 +26,20 @@ for element in soup.select('.article__content'):
         parsed_date = parser.parse(date_string)
         date = parsed_date.strftime("%Y-%m-%d")
 
-        headline = currNews
-        print(headline)
+        headline = currNews[:-2]
+
+        result = []
+        current_word = ""
+
+        for element in headline:
+            if element.strip() == "":
+                continue  # Skip empty strings and newline characters
+            current_word += element.strip() + " "
+
+        if current_word != "":
+            result.append(current_word.strip())
+
+        print(result)  
+        #print(headline)
         print(date)
     #print("\n\n\n")
